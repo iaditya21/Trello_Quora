9388435f74a2b419f5cee51959593a2dbea8b453
iaditya21 <ishanaditya21@gmail.com>
2020-10-10 19:13:52 +0530

    Exception handling when user has singed out
    Signed-off-by: iaditya21 <ishanaditya21@gmail.com>

--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -19,6 +19,18 @@ public class CommonController {
     @Autowired
     private UserService userService;
 
+    /**
+     * Method that returns user details as response
+     * Input-
+     *          userId-Unique user uuid
+     *              Type-String
+ *              authToken-Unique sessions auth token
+     *               Type-String
+     *
+     *  Output:
+     *      UserDetailsResponse along with status code is returned for a valid set of input otherwise error message is returned
+     *
+     * */
     @RequestMapping(method = RequestMethod.GET,path="/userprofile/{userId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<UserDetailsResponse> userProfile(@PathVariable("userId") final String userId,
                                                            @RequestHeader ("authorization") final String authToken) throws AuthorizationFailedException, UserNotFoundException {
--- quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
+
 @Service
 public class UserService {
 
@@ -19,16 +22,39 @@ public class UserService {
     @Autowired
     private  PasswordCryptographyProvider passwordCryptographyProvider;
 
+    /**
+     * Checks the user,if valid user is found then user details are returned
+     *
+     * Input:
+     *      uuid:
+     *          Type-String
+     *          Unique uuid to identify user
+ *          authToken
+     *           Type-String
+     *           Unique authToken to identify a session
+     *
+     * */
     @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity getUser(String uuid,String authToken) throws AuthorizationFailedException, UserNotFoundException {
        UserAuthTokenEntity authTokenEntity= userDao.getAuthToken(authToken);
+       //Checks if authToken is valid or not.
        if(authTokenEntity==null){
             throw new AuthorizationFailedException("ATHR-001","User has not signed in.");
        }
        UserEntity user= userDao.getUser(uuid);
+        //Checks if user is valid or not.
        if(user==null){
            throw new UserNotFoundException("USR-001","User with entered uuid does not exist");
        }
+
+        LocalDateTime logoutTime=authTokenEntity.getLogoutAt().toLocalDateTime();
+        LocalDateTime currentTime=LocalDateTime.now();
+        //Checks  logged out time to determine if user is currently signed in or not.
+       if(logoutTime.isBefore(currentTime))
+       {
+           throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get user details");
+       }
        return user;
     }
+
 }
