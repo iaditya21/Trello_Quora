9817a1ec853f69ae692c2fdab2703dffc6312ab5
iaditya21 <ishanaditya21@gmail.com>
2020-10-11 16:13:21 +0530

    Question service to create a new question
    Signed-off-by: iaditya21 <ishanaditya21@gmail.com>

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -0,0 +1,48 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private  PasswordCryptographyProvider passwordCryptographyProvider;
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity createQuestion(QuestionEntity question, String authToken) throws AuthorizationFailedException {
+       UserAuthTokenEntity authTokenEntity= userDao.getAuthToken(authToken);
+       //Checks if authToken is valid or not.
+       if(authTokenEntity==null){
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in.");
+       }
+
+        LocalDateTime logoutTime=authTokenEntity.getLogoutAt().toLocalDateTime();
+        LocalDateTime currentTime=LocalDateTime.now();
+        //Checks  logged out time to determine if user is currently signed in or not.
+       if(logoutTime.isBefore(currentTime))
+       {
+           throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get user details");
+       }
+
+       return questionDao.createQuestion(question);
+    }
+
+}
